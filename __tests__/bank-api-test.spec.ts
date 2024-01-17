49e8f2a0efa419c32b23f2f52218da6ab8eb8ff8
Tomer Marshall <tomermarshall2000@gmail.com>
2024-01-17 16:43:43 +0200

    added test packages and made config file and test file

---
+++ __tests__/bank-api-test.spec.ts
@@ -0,0 +1,23 @@
+import {afterEach, beforeEach} from "node:test";
+
+beforeEach(() => {
+
+});
+
+afterEach(() => {
+
+});
+
+describe('Bank Api Testing', () => {
+    describe('Endpoints test', () => {
+
+    });
+
+    describe('Validation', () => {
+
+    });
+
+    describe('Sql injection', () => {
+
+    });
+});
